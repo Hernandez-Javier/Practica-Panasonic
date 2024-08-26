a62913e8cbbefcb9c3b30cb2c7a002c8166f876b
Javier-Hernandez <javihl14@gmail.com>
2024-08-26 11:09:29 -0600

    improvemebt on routing

--- index.js
+++ index.js
@@ -804,6 +804,12 @@ app.get('/bitacora/all', async (req, res) => {
   }
 });
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
