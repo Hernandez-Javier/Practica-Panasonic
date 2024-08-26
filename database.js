c1580d977111661e3ab5b1f5608e99cf880f66d3
Javier-Hernandez <javihl14@gmail.com>
2024-08-26 01:25:34 -0600

    db env changes

--- database.js
+++ database.js
@@ -4,9 +4,9 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: process.env.DB_user,
   host: process.env.DB_host,
-  database: 'panasonicss',
+  database: process.env.DB_name,
   password: process.env.DB_pass,
-  port: 5432,
+  port: process.env.DB_port,
 });
 
 module.exports = pool;
