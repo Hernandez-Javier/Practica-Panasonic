f7dc4e1157f65507f6e601e750f8ccd9fc7a6374
Javier-Hernandez <javihl14@gmail.com>
2024-08-26 08:37:10 -0600

    Full check
    test backend after changes

--- config/database.js
+++ config/database.js
@@ -1,12 +1,16 @@
 // configuracion a db
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'Bodega_Panasonic',
+  user: process.env.DB_user,
+  host: process.env.DB_host,
+  database: process.env.DB_name,
   password: process.env.DB_pass,
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
